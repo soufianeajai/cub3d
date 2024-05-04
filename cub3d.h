81bb541db8710558b25144a1dad091873a2f10fe
Soufiane Ajaite <sajaite@e2r5p6.1337.ma>
2024-05-04 01:25:01 +0100

    just added some consts

--- cub3d.h
+++ cub3d.h
@@ -7,8 +7,10 @@
 # include<math.h>
 # include<stdio.h>
 # include<unistd.h>
-# define HEIGHT 1000
-# define WIDTH 1000
+# define HEIGHT 1024
+# define WIDTH 1024
+# define PI	3.1416
+# define SIZE_CUBE 64
 
 typedef struct s_input {
   char  *no;
@@ -39,6 +41,7 @@ typedef struct s_mlx {
 	t_img		image;
 
 }	t_mlx;
+
 int	ft_connect(t_mlx *mlx);
 void	my_mlx_pixel_put(t_img *image, int x, int y, int color);
 void	ft_error(void	*ptr, char *msg);
